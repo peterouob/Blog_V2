3d78570976a0e9b3dd7d6147a98596cf872d9f03
林庭楷 <hellolinpeter@gmail.com>
2024-10-05 02:56:44 +0800

    finial

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -46,6 +46,7 @@ export default async function BlogPage({searchParams}:BlogPageProps){
                                         title={title}
                                         description={description}
                                         tags={tags}
+                                        key={slug}
                                     />
                                 </li>
                             </ul>
