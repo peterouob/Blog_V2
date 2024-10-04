ea4d17faf43d24810d75c7410428510830a15ec9
林庭楷 <hellolinpeter@gmail.com>
2024-10-04 18:41:37 +0800

    better main page

--- components/AuroraBackgroundDemo.tsx
+++ components/AuroraBackgroundDemo.tsx
@@ -22,9 +22,15 @@ export function AuroraBackgroundDemo() {
                    }}
                    className="relative flex flex-col gap-4 items-center justify-center px-4"
                >
-                   <div className="text-4xl md:text-7xl font-bold dark:text-white text-center flex">
-                       Welcome to my Blog
+                   <div>
+                       <div className="text-4xl md:text-7xl font-bold dark:text-white text-center flex">
+                           Welcome to my Blog
+                       </div>
+                       <div className="text-zinc-400 tracking-wide leading-relaxed lg:text-2xl text-center mt-8 :text-xl">
+                           Here is to record my life
+                       </div>
                    </div>
+
                    <FloatingDockDemo />
                    <div className="container">
                        <ThemeToggle />
